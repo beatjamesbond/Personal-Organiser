7f22b55ed855369a1d323cafa5565d5e72df1354
ljames96 <lwjames1996@gmail.com>
2015-03-25 11:33:30 +0000

    Update Create.java

--- PIM/src/Contacts/Contact/Create.java
+++ PIM/src/Contacts/Contact/Create.java
@@ -1,5 +1,18 @@
 package Contacts.Contact;
 
-public class Create {
+import Contacts.Logic;
 
+public class Create {
+	
+	private Logic logic;
+	
+	public void createContact(String[] contactDetails){
+		String contactName = contactDetails[0];
+		String contactNumber = contactDetails[1];
+		String contactEmail = contactDetails[2];
+		String contactAddress = contactDetails[3];
+		String contactGroup = contactDetails[4];
+		Contact c = new Contact(contactName, contactNumber, contactEmail, contactAddress, contactGroup);
+		logic.addContact(c);
+	}
 }
